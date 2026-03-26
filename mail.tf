abed26b74c8a358c9c516c3f1e60c9077d9a7c28
JOAO PAULO SAPI DE PAULA <joao.paula@corenmg.gov.br>
2026-03-26 15:45:03 -0300

    Resources To Azure Using Terraform

--- mail.tf
+++ mail.tf
@@ -2,25 +2,41 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.31.1"
+      version = "~> 3.0"
     }
   }
+
+  # COMENTE ESTE BLOCO AGORA
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-terraform-state"
+  #   storage_account_name = "sttfstatecorenmg"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
   features {}
-
 }
 
-resource "azurerm_resource_group" "rg_hml_app" {
-  name     = "rg-hml-app"
+# 1. Recurso do Resource Group
+resource "azurerm_resource_group" "state-rg" {
+  name     = "rg-terraform-state"
   location = "southcentralus"
-  tags = {
-    environment = "dev"
-    source      = "Terraform"
-    owner       = "Joao Paulo"
-  }
-
 }
 
+# 2. Recurso do Storage Account
+resource "azurerm_storage_account" "state-sa" {
+  name                     = "sttfstatecorenmg"
+  resource_group_name      = azurerm_resource_group.state-rg.name
+  location                 = azurerm_resource_group.state-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
+# 3. Recurso do Container
+resource "azurerm_storage_container" "state-container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.state-sa.name
+  container_access_type = "private"
+}
